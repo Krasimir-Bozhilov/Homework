e491d540ae523d7091e3ffdc458053f294f5a915
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2020-11-04 11:11:15 +0200

    Homework

--- src/Hero/Main.java
+++ src/Hero/Main.java
@@ -3,24 +3,6 @@ package Hero;
 public class Main {
     public static void main(String[] args) {
 
-        Hero hero=new Hero("Krasi", 101);
-        System.out.println(hero.toString());
-        Elf elf =new Elf("Krasi", 101);
-        System.out.println(elf.toString());
-        Wizard wizard =new Wizard("Krasi", 101);
-        System.out.println(wizard.toString());
-        Knight knight=new Knight("Krasi", 101);
-        System.out.println(knight.toString());
-        MuseElf museElf=new MuseElf("Krasi", 101);
-        System.out.println(museElf.toString());
-        DarkWizard darkWizard =new DarkWizard("Krasi", 101);
-        System.out.println(darkWizard.toString());
-        DarkKnight darkKnight=new DarkKnight("Krasi", 101);
-        System.out.println(darkKnight.toString());
-        SoulMaster soulMaster=new SoulMaster("Krasi", 101);
-        System.out.println(soulMaster.toString());
-        BladeKnight bladeKnight = new BladeKnight("Krasi", 101);
-        System.out.println(bladeKnight.toString());
 
     }
 }
