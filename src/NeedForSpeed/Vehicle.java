774cd661def7cdaa157410c24c3ffd75efad1595
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2020-11-05 10:12:56 +0200

    Homework

---
+++ src/NeedForSpeed/Vehicle.java
@@ -0,0 +1,10 @@
+package NeedForSpeed;
+
+public class Vehicle {
+    private  final static double  DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+
+}
