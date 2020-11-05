d79e38dc0cb7583560fddb9cb33533a94ded4cc6
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2020-11-05 15:11:29 +0200

    Homework

--- src/NeedForSpeed/Vehicle.java
+++ src/NeedForSpeed/Vehicle.java
@@ -2,9 +2,41 @@ package NeedForSpeed;
 
 public class Vehicle {
     private  final static double  DEFAULT_FUEL_CONSUMPTION = 1.25;
-    private double fuelConsumption;
+    private double fuelConsumption=DEFAULT_FUEL_CONSUMPTION;
     private double fuel;
     private int horsePower;
 
+    public Vehicle (double fuel, int horsePower){
+        this.fuel=fuel;
+        this.horsePower=horsePower;
+    }
+    public void drive(double kilometers){
+        if (fuel>0){
+            fuel=fuel-(kilometers*fuelConsumption);
+        }
+    }
 
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
 }
