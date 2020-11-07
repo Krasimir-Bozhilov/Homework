79cc76538c2327e94bce815f70d6ffae9901505f
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2020-11-07 17:51:24 +0200

    Homework

---
+++ src/NeedForSpeed/RaceMotorcycle.java
@@ -0,0 +1,13 @@
+package NeedForSpeed;
+
+public class RaceMotorcycle extends Motorcycle {
+    private  final static double  DEFAULT_FUEL_CONSUMPTION = 8;
+    private double fuelConsumption=DEFAULT_FUEL_CONSUMPTION;
+    public RaceMotorcycle(double fuel,int horsePower){
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+
+
+
+}
